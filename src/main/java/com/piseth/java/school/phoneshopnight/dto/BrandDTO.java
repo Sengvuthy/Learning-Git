ca79638305a2f2fc5fef9018f6d6852df54cb1be
Vuthy Seng <sengvuthy1414@gmail.com>
2024-11-10 21:58:13 +0700

    Initialize Project

---
+++ src/main/java/com/piseth/java/school/phoneshopnight/dto/BrandDTO.java
@@ -0,0 +1,9 @@
+package com.piseth.java.school.phoneshopnight.dto;
+
+import lombok.Data;
+
+@Data
+public class BrandDTO {
+	//private Integer id;
+	private String name;
+}
---
+++ src/main/java/com/piseth/java/school/phoneshopnight/service/BrandService.java
@@ -0,0 +1,7 @@
+package com.piseth.java.school.phoneshopnight.service;
+
+import com.piseth.java.school.phoneshopnight.entity.Brand;
+
+public interface BrandService {
+	Brand create(Brand brand);
+}
